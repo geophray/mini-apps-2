8341ffd9611ae59c4eb8a06ad3d1a3b21deae263
Jeff Maughan <geophray@gmail.com>
2020-06-10 10:44:59 -0700

    Add webpack config file

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve('./public'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+      },
+    ],
+  },
+};
