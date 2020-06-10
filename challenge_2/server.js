81087c2103b1e50a52d38f9112cc2ac2b32f7c71
Jeff Maughan <geophray@gmail.com>
2020-06-10 11:46:10 -0700

    Add /coindesk api endpoint

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const genDate = require('./genDate.js');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -12,6 +14,19 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/test', (req, res) => {
   res.send('Get request received at "/test" endpoint');
 });
+
+app.get('/coindesk', (req, res) => {
+  const today = genDate();
+  axios
+    .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=${today}`)
+    .then((results) => {
+      res.status(200).send(results.data);
+    })
+    .catch((err) => {
+      console.error('Error retrieving data from coindesk api.\n', err);
+      res.status(500).send('Error retrieving data.');
+    });
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
