53297400dd73ae0087e5e81dfa01a5b1fc5eb7e5
Jeff Maughan <geophray@gmail.com>
2020-06-11 11:29:50 -0700

    Add basic webpack config

---
+++ challenge_3/client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client/app.jsx')
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'app'),
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
