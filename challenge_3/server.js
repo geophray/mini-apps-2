cd18a5d199cedfc9bf5efd840553d3a07d9a523a
Jeff Maughan <geophray@gmail.com>
2020-06-11 11:42:26 -0700

    Add basic express server settings

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3003;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
